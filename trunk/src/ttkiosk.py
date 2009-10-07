9dea777619a0e0e8eeaeb5a4b5e9efe4b3260598
sergey.satskiy <sergey.satskiy@0cd7ca3e-a1af-11de-ba2c-3b65426ee844>
2009-10-07 03:49:49 +0000

    Fix: unhandled exception handler

--- trunk/src/ttkiosk.py
+++ trunk/src/ttkiosk.py
@@ -28,7 +28,7 @@ It performs necessery initialization and starts the Qt main loop.
 
 __version__ = "0.0.1"
 
-import utils, mail, sys, os.path
+import utils, mail, sys, os.path, traceback
 from PyQt4 import QtGui
 from optparse import OptionParser
 
@@ -62,17 +62,17 @@ def ttkioskMain():
     try:
         settings = utils.Settings()
     except:
-        message = utils.getExceptionInfo()
         print >> sys.stderr, "Error loading settings. " \
                              "Fix the error and run again."
         sys.exit( 1 )
 
     # Settings have been loaded successfully.
     # Now the default exception handler can be replaced
-    # sys.excepthook = exceptionHook
+    sys.excepthook = exceptionHook
 
     globalData = utils.GlobalData()
     ttkioskApp = QtGui.QApplication( sys.argv )
+    globalData.application = ttkioskApp
 
     screenSize = ttkioskApp.desktop().screenGeometry()
     globalData.screenWidth = screenSize.width()
@@ -92,37 +92,58 @@ def ttkioskMain():
 
     # Show startup forms; the startup forms may not have arguments
     for formName in globalData.startupForms:
+        if formName == 'DebugBar':
+            if not utils.debug:
+                continue
         ui.showForm( formName )
 
     # Run the application main cycle
     ttkioskApp.exec_()
 
 
-def exceptionHook( excType, excValue, tracebackobj ):
+def exceptionHook( excType, excValue, tracebackObj ):
     """ Catches unhandled exceptions """
+
+    globalData = utils.GlobalData()
+
+    # Keyboard interrupt is a special case
+    if issubclass( excType, KeyboardInterrupt ):
+        if not globalData.application is None:
+            globalData.application.quit()
+        return
+
+    filename, line, dummy, dummy = traceback.extract_tb( tracebackObj ).pop()
+    filename = os.path.basename( filename )
+    error    = "%s: %s" % ( excType.__name__, excValue )
+    stackTraceString = "".join( traceback.format_exception( excType, excValue,
+                                                            tracebackObj ) )
 
     # Write a message to a log file
     utils.writeToLog( "Unhandled exception is caught\n" + \
-                      utils.getExceptionInfo() )
+                      stackTraceString )
 
-    print >> sys.stderr, "Unhandled exception is caught\n" + \
-                         utils.getExceptionInfo()
+    print >> sys.stderr, "Unhandled exception is caught\n"
+    print >> sys.stderr, stackTraceString
 
     # Display the message as a QT modal dialog box if the application
     # has started
+    if not globalData.application is None:
+        QtGui.QMessageBox.critical( None, "Error: " + error,
+                                    "<html>Unhandled exception is caught." \
+                                    "<pre>" + stackTraceString + "</pre>" \
+                                    "</html>" )
 
-
-    # TODO: e-mail sending is better done as asynch process because it
-    #       causes delays especially if the connection is broken
     # Send an e-mail
     try:
         mail.sendErrorReport( "ttkiosk: unhandled exception",
                               "Unhandled exception is caught\n" + \
-                              utils.getExceptionInfo() + "\n" + \
+                              stackTraceString + "\n" + \
                               utils.collectHostInfo() )
     except:
         utils.writeToLog( "Cannot send e-mail error report: " + \
-                          utils.getExceptionInfo() )
+                          stackTraceString )
+    sys.exit( 1 )
+
 
 if __name__ == '__main__':
     ttkioskMain()
